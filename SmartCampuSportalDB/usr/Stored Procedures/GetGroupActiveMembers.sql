f2e9131da1346dcf60ddc85c40a59da4e7fb5a49
excellent mashengete <excellent@digitalsolutionfoundry.ac.za>
2025-04-29 03:34:36 +0200

    remove uncertain fields

--- SmartCampuSportalDB/usr/Stored Procedures/GetGroupActiveMembers.sql
+++ SmartCampuSportalDB/usr/Stored Procedures/GetGroupActiveMembers.sql
@@ -8,13 +8,10 @@ SELECT
 	[G].Description,
 	[GM].StakeholderId,
 	[U].Username,
-	[SP].FirstName,
-	[SP].LastName,
-	[SP].TitleId
+	[S].Name
 FROM usr.[GroupMember] [GM]
 INNER JOIN usr.[Group] [G] ON [G].GroupId = [GM].GroupId
 INNER JOIN [usr].[User] [U] ON [U].StakeholderId = [GM].StakeholderId
 INNER JOIN sh.[Stakeholder] [S] ON [S].StakeholderId = [GM].StakeholderId
-INNER JOIN [sh].StakeholderPerson [SP] ON [SP].StakeholderId = [S].StakeholderId
 WHERE [GM].GroupId = @groupId OR @groupId IS NULL
 AND [G].IsDeleted = 0 AND [U].IsDeleted =0
---
+++ SmartCampuSportalDB/usr/Stored Procedures/GetSecurityUser.sql
@@ -0,0 +1,17 @@
+CREATE PROCEDURE [usr].[GetSecurityUser]
+	@stakeholderId	INT = NULL,
+	@userName		NVARCHAR(255) = NULL
+AS
+SELECT 
+	[U].StakeholderId,
+	[U].Username,
+    [U].PasswordHash,
+    [U].SecurityStamp,
+	[U].IsDeleted,
+	[U].IsLocked,
+    [SH].Name
+FROM [usr].[User] [U]
+INNER JOIN [sh].Stakeholder SH ON [SH].StakeholderId = [U].StakeholderId
+INNER JOIN [sh].StakeholderPerson [SP] ON [SP].StakeholderId = [SH].StakeholderId
+WHERE (@stakeholderId IS NOT NULL AND [U].StakeholderId = @stakeholderId)
+   OR (@userName IS NOT NULL AND [U].Username = @userName)

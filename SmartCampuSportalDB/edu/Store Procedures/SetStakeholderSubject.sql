febcffa9fbeee8b38e378747ff249296fffb75a5
excellent mashengete <excellent@digitalsolutionfoundry.ac.za>
2025-05-04 12:36:41 +0200

    rename tables

--- SmartCampuSportalDB/edu/Store Procedures/SetStakeholderSubject.sql
+++ SmartCampuSportalDB/edu/Store Procedures/SetStakeholderSubject.sql
@@ -1,30 +1,30 @@
-CREATE PROCEDURE [edu].[setStudentSubject]
+CREATE PROCEDURE [edu].[SetStakeholderSubject]
     @subjectId              INT,
     @stakeholderId          INT,
     @isDeleted              BIT = 0
 AS
 BEGIN
     SET NOCOUNT ON;
-    IF EXISTS(SELECT SubjectId FROM edu.StudentSubject 
+    IF EXISTS(SELECT SubjectId FROM edu.StakeholderSubject 
         WHERE SubjectId = @subjectId 
         AND StakeholderId = @stakeholderId
         AND IsDeleted = 0)
     BEGIN
-        UPDATE edu.StudentSubject 
+        UPDATE edu.StakeholderSubject 
         SET IsDeleted = @isDeleted
         WHERE SubjectId = @subjectId 
         AND StakeholderId = @stakeholderId
     END
     ELSE
     BEGIN
-        INSERT INTO edu.StudentSubject (SubjectId, StakeholderId, IsDeleted)
+        INSERT INTO edu.StakeholderSubject (SubjectId, StakeholderId, IsDeleted)
         VALUES (@subjectId, @stakeholderId, @isDeleted);
     END;
     SELECT
        [E].SubjectId,
        [E].StakeholderId,
        [E].IsDeleted
-    FROM edu.StudentSubject [E]
+    FROM edu.StakeholderSubject [E]
     WHERE [E].StakeholderId = @stakeholderId
     AND [E].SubjectId = @subjectId
     AND [E].IsDeleted = @isDeleted

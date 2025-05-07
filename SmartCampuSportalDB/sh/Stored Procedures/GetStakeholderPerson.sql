40d8386bc4cd705a7f5f1ed83d902e381e1b1b2b
excellent mashengete <excellent@digitalsolutionfoundry.ac.za>
2025-05-07 18:18:36 +0200

    update

--- SmartCampuSportalDB/sh/Stored Procedures/GetStakeholderPerson.sql
+++ SmartCampuSportalDB/sh/Stored Procedures/GetStakeholderPerson.sql
@@ -9,7 +9,7 @@ SELECT
     [SHP].LastName,
     [SHP].TitleId
 FROM [sh].StakeholderPerson [SHP]
-INNER JOIN [sh].Title [T] ON [SHP].TitleId = [T].TitleId 
+LEFT JOIN [sh].Title [T] ON [SHP].TitleId = [T].TitleId 
 INNER JOIN [sh].Stakeholder [SH] ON [SHP].StakeholderId = [SH].StakeholderId
 WHERE [SHP].StakeholderId = @stakeholderId
 AND SH.IsDeleted = 0

fe3d8c7551bba8f2b298dc724f5a8a5f46cc762f
excellent mashengete <excellent@digitalsolutionfoundry.ac.za>
2025-05-08 19:25:39 +0200

    fix build

--- SmartCampuSportalDB/svc/Stored Procedures/GetStakeholderMaintenanceRequest.sql
+++ SmartCampuSportalDB/svc/Stored Procedures/GetStakeholderMaintenanceRequest.sql
@@ -4,15 +4,15 @@ CREATE PROCEDURE [svc].[GetStakeholderMaintenanceRequest]
 AS
 SET NOCOUNT ON;
 
-SELECT [IssueId]
-      ,[StakeholderId]
-      ,[Title]
-      ,[RoomId]
-      ,[Description]
-      ,[StatusId]
-      ,[DateReported]
-      ,[DateResolved]
-FROM [svc].[MaintenanceIssue]
+SELECT M.[IssueId]
+      ,M.[StakeholderId]
+      ,M.[Title]
+      ,M.[RoomId]
+      ,M.[Description]
+      ,M.[StatusId]
+      ,M.[DateReported]
+      ,M.[DateResolved]
+FROM [svc].[MaintenanceIssue] M
 INNER JOIN svc.[Status] S ON M.StatusId = S.StatusId
 WHERE (@stakeholderId IS NULL OR StakeholderId = @stakeholderId)
 AND (@statudId IS NULL OR S.StatusId IN (SELECT value FROM STRING_SPLIT(@statudId, ',')))

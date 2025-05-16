0df45117142b49df67b31c6f93c64c2ebcc0dd0c
Excellent-Mashengete <mashengete@live.com>
2025-05-16 22:37:26 +0200

    update

--- SmartCampuSportalDB/ntf/Stored Procedures/MarkNotificationAsRead.sql
+++ SmartCampuSportalDB/ntf/Stored Procedures/MarkNotificationAsRead.sql
@@ -1,40 +1,17 @@
 CREATE PROCEDURE [ntf].[MarkNotificationAsRead]
-    @stakeholderId INT,
-    @notificationId INT
-AS
+    @stakeholderId INT
+AS 
 BEGIN
-SET NOCOUNT ON;
-    IF EXISTS (SELECT 1 
-        FROM [ntf].[NotificationView] 
-        WHERE StakeholderId = @stakeholderId 
-        AND NotificationId = @notificationId 
-        AND IsRead = 0
-    )
+    SET NOCOUNT ON;
 
-    BEGIN
-        UPDATE [ntf].[NotificationView]
-        SET IsRead = 1
-        WHERE StakeholderId = @stakeholderId
-        AND NotificationId = @notificationId;
-    END
-    
-    SELECT 
-        N.NotificationId,
-        N.SenderId,
-        SH.Name,
-        N.NotificationTypeId,
-        N.SubjectId 'RelatedSubject',
-        N.Message,
-        NV.IsRead,
-        N.DateCreated,
-        N.DateUpdated,
-        S.SubjectId,
-        S.SubjectCode,
-        S.SubjectName
-    FROM [ntf].NotificationView NV
-    INNER JOIN [ntf].Notification N ON NV.NotificationId = n.NotificationId
-    INNER JOIN [sh].Stakeholder SH ON N.SenderId = SH.StakeholderId
-    LEFT JOIN [edu].Subject S ON  N.SubjectId = S.SubjectId
-    WHERE NV.NotificationId = @notificationId
-    AND NV.StakeholderId = @stakeholderId
+    DECLARE @UpdatedRows INT;
+
+    UPDATE [ntf].[NotificationView]
+    SET IsRead = 1
+    WHERE StakeholderId = @stakeholderId
+      AND IsRead = 0;
+
+    SET @UpdatedRows = @@ROWCOUNT;
+
+    SELECT CAST(IIF(@UpdatedRows > 0, 1, 0) AS BIT) AS Success;
 END

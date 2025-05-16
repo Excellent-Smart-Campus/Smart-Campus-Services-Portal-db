d212dcc96c43e2b88196b843682a0d1c7de165e5
Excellent-Mashengete <mashengete@live.com>
2025-05-16 23:42:31 +0200

    updates

--- SmartCampuSportalDB/svc/Stored Procedures/SetBooking.sql
+++ SmartCampuSportalDB/svc/Stored Procedures/SetBooking.sql
@@ -38,6 +38,5 @@ SELECT
     [B].DateCreated
 FROM svc.Booking [B]
 INNER JOIN [sh].Stakeholder [S] ON [B].StakeholderId = [S].StakeholderId
-INNER JOIN [svc].Room [R] ON [B].RoomId = [R].RoomId
 INNER JOIN [svc].[Status] [ST] ON [B].[StatusId] = [ST].StatusId
 WHERE [B].BookingId = @bookingId
